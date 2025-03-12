22af13a4e3aad3a6784da2dde0d28d8df804200b
Ente <ente@openducks.org>
2025-03-12 19:22:18 +0100

    TT-91: New GUI for login and password reset page

--- suite/forgot_password.php
+++ suite/forgot_password.php
@@ -5,23 +5,60 @@ use Arbeitszeit\Arbeitszeit;
 $arbeitszeit = new Arbeitszeit;
 $ini = $arbeitszeit->get_app_ini();
 $loc = $arbeitszeit->i18n()->loadLanguage(NULL, "reset");
-echo $arbeit->check_status_code($_SERVER["REQUEST_URI"]);
+echo $arbeitszeit->check_status_code($_SERVER["REQUEST_URI"]);
+
 ?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <title><?php echo $loc["title"] ?> | <?php echo $ini["general"]["app_name"]; ?></title>
-        <link rel="stylesheet" type="text/css" href="/assets/css/index.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
+<html data-bs-theme="dark" lang="en">
 
-        <form class="box" action="actions/auth/reset.php" method="POST">
-            <h2><?php echo $loc["title_q"] ?></h2>
-            <p><?php echo $loc["request_mail"] ?></p>
-            <label><?php echo $loc["label_email"] ?>: </label><input type="email" name="email" placeholder="you@mail.com">
-            <br>
-            <button class="button" type="submit" name="reset" value="true"><?php echo $loc["label_button"] ?></button>
-        </form>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title><?php echo $loc["title"] ?> | <?php echo $ini["general"]["app_name"]; ?></title>
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/Dark-Mode-Switch.css">
+    <link rel="stylesheet" href="/assets/css/Navbar-Centered-Brand-icons.css">
+    <link rel="stylesheet" href="/assets/css/Pretty-Registration-Form-.css">
+</head>
+
+<body>
+    <section class="position-relative py-4 py-xl-5">
+        <div class="container position-relative">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5 col-xxl-6">
+                    <div class="card mb-5">
+                        <div class="card-body p-sm-5">
+                            <h2 class="text-center mb-4"><?php echo $loc["title_q"] ?></h2>
+                            <p class="text-center mb-4"><?php echo $loc["request_mail"] ?></p>
+                            <form method="post" action="actions/auth/reset.php">
+                                <div class="mb-3"><input class="form-control" type="email" id="email-2" name="email" placeholder="<?php echo $loc["label_email"] ?>"></div>
+                                <div><button class="btn btn-primary d-block w-100" type="submit"><?php echo $loc["label_button"] ?></button></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="text-center py-4">
+        <div class="container" style="max-height: none;">
+            <div class="row row-cols-1 row-cols-lg-3">
+                <div class="col">
+                    <p class="text-muted my-2"><?php echo $ini["general"]["app_name"]; ?> - <?php echo date("Y") ?> -&nbsp;<a href="https://github.com/Ente/timetrack/releases/tag/v<?php echo @file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/VERSION"); ?>" target="_blank" rel="external">TimeTrack</a></p>
+                </div>
+                <div class="col"></div>
+                <div class="col">
+                    <ul class="list-inline my-2">
+                        <li class="list-inline-item"><a class="link-secondary" href="#">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a class="link-secondary" href="#">Cookies</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/js/bs-init.js"></script>
+    <script src="../assets/js/Dark-Mode-Switch-darkmode.js"></script>
+</body>
+
 </html>
--- suite/login.php
+++ suite/login.php
@@ -8,25 +8,59 @@ $base_url = $ini["general"]["base_url"];
 echo $arbeit->check_status_code($_SERVER["REQUEST_URI"]);
 $language = $arbeit->i18n()->loadLanguage(NULL, "login");
 ?>
+
 <!DOCTYPE html>
-<html>
-    <head>
-        <title><?php echo $language["title"] ?> | <?php echo $ini["general"]["app_name"]; ?></title>
-        <link rel="stylesheet" type="text/css" href="/assets/css/index.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <h1><?php echo $language["h1"] ?></h1>
-        <form class="box" action="actions/auth/login.php" method="POST">
-            <label><?php echo $language["label_username"] ?>: </label><input class="input" type="text" name="username" placeholder="<?php echo $language["placeholder_username"] ?>">
-            <br><br>
-            <label><?php echo $language["label_password"] ?>: </label><input class="input" type="password" name="password" placeholder="<?php echo $language["placeholder_password"] ?>">
-            <br>
-            <button class="button" type="submit" name="submit"><?php echo $language["button_text"] ?></button>
-            <br>
-            <input type="checkbox" name="erinnern"><label title="Wenn das Häkchen gesetzt wird, wirst du für die nächsten 30 Tage automatisch angemeldet."><?php echo $language["checkbox_30days"] ?></label>
-        </form>
-        <br>
-        <a href="forgot_password.php"><?php echo $language["forgot_pw"] ?></a>
-    </body>
+<html data-bs-theme="dark" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title><?php echo $language["title"] ?> | <?php echo $ini["general"]["app_name"]; ?></title>
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/Dark-Mode-Switch.css">
+    <link rel="stylesheet" href="/assets/css/Navbar-Centered-Brand-icons.css">
+    <link rel="stylesheet" href="/assets/css/Pretty-Registration-Form-.css">
+</head>
+
+<body>
+    <section class="position-relative py-4 py-xl-5">
+        <div class="container position-relative">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5 col-xxl-6">
+                    <div class="card mb-5">
+                        <div class="card-body p-sm-5">
+                            <h2 class="text-center mb-4"><?php echo $language["h1"] ?></h2>
+                            <form method="post" action="actions/auth/login.php">
+                                <div class="mb-3"><input class="form-control" type="text" id="email-1" name="username" placeholder="<?php echo $language["placeholder_username"] ?>" style="margin-bottom: 5px;"><input class="form-control" type="password" id="email-2" name="password" placeholder="<?php echo $language["placeholder_password"] ?>" style="margin-bottom: 5px;"></div>
+                                <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-1" name="erinnern"><label class="form-check-label" for="formCheck-1"><?php echo $language["checkbox_30days"] ?></label></div>
+                                <div><button class="btn btn-primary d-block w-100" type="submit"><?php echo $language["button_text"] ?></button></div>
+                            </form>
+                        </div>
+                        <a href="forgot_password.php"><?php echo $language["forgot_pw"] ?></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="text-center py-4">
+        <div class="container" style="max-height: none;">
+            <div class="row row-cols-1 row-cols-lg-3">
+                <div class="col">
+                    <p class="text-muted my-2"><?php echo $ini["general"]["app_name"]; ?> - <?php echo date("Y") ?> -&nbsp;<a href="https://github.com/Ente/timetrack/releases/tag/v<?php echo @file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/VERSION"); ?>" target="_blank" rel="external">TimeTrack</a></p>
+                </div>
+                <div class="col"></div>
+                <div class="col">
+                    <ul class="list-inline my-2">
+                        <li class="list-inline-item"><a class="link-secondary" href="#">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a class="link-secondary" href="#">Cookies</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/js/bs-init.js"></script>
+    <script src="../assets/js/Dark-Mode-Switch-darkmode.js"></script>
+</body>
+
 </html>
