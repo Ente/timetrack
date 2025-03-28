9ac7cf5271188f1ecc94de836b709691d976c1ba
Ente <ente@openducks.org>
2025-03-28 13:29:35 +0100

    TT-85: Differentiate API endpoints between admin, internal and public

--- api/v1/toil/Routes.toil.arbeit.inc.php
+++ api/v1/toil/Routes.toil.arbeit.inc.php
@@ -15,7 +15,8 @@ use Arbeitszeit\Exceptions;
 use Toil\Controller;
 use Toil\Permissions;
 
-class Routes extends Toil {
+class Routes extends Toil
+{
     private Arbeitszeit $arbeitszeit;
     private Benutzer $benutzer;
 
@@ -29,9 +30,22 @@ class Routes extends Toil {
 
     private $bString = "Toil API";
 
-    public function __construct(){
-        $user = $_SERVER["PHP_AUTH_USER"];
-        $pw = $_SERVER["PHP_AUTH_PW"];
+    public function __construct()
+    {
+        $this->__set("eventHandler", new EventHandler());
+
+        $endpoint = preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']);
+        $endpointName = $this->getResourceNameFromPath($endpoint);
+        $permissions = new Permissions();
+
+        if ($permissions->checkPermissions(null, $endpointName)) {
+            Exceptions::error_rep("[API] Public access to '$endpointName' allowed – skipping authentication.");
+            return;
+        }
+
+        $user = $_SERVER["PHP_AUTH_USER"] ?? null;
+        $pw = $_SERVER["PHP_AUTH_PW"] ?? null;
+
         $this->__set("arbeitszeit", new Arbeitszeit());
         $this->__set("benutzer", new Benutzer());
         if (!$user) {
@@ -40,34 +54,37 @@ class Routes extends Toil {
         } else {
             $this->__set("api_username", $user);
         }
-        
+
         $this->__set("api_password", $pw or $this->authError($user));
 
-        if(!$this->login(username: $user, password: $pw)){
+        if (!$this->login(username: $user, password: $pw)) {
             $this->authError($user);
         }
-        $this->__set("eventHandler", new EventHandler());
     }
 
-    public function __set($name, $value){
+    public function __set($name, $value)
+    {
         $this->$name = $value;
     }
 
-    public function __get($name){
+    public function __get($name)
+    {
         return $this->$name;
     }
 
-    public function authError($name = null){
+    public function authError($name = null)
+    {
         Exceptions::error_rep("[API] Failed authentication for Toil API for user '{$name}'");
         header("WWW-Authenticate: Basic realm='" . $this->bString . "'");
         header("HTTP/1.0 401 Unauthorized");
         die("Not authenticated");
     }
 
-    public function login($username, $password){
-        if($this->__get("benutzer")->get_user($username)){
+    public function login($username, $password)
+    {
+        if ($this->__get("benutzer")->get_user($username)) {
             $data = $this->__get("benutzer")->get_user($username);
-            if(password_verify($password, $data["password"])){
+            if (password_verify($password, $data["password"])) {
                 Exceptions::error_rep("[API] Successfully authenticated user '$username' via API.");
                 return true;
             }
@@ -76,16 +93,17 @@ class Routes extends Toil {
         }
     }
 
-    public function routing($eventHandler){
+    public function routing($eventHandler)
+    {
         Exceptions::error_rep("[API] Start API routing request and registering routes...");
         $base = $this->__get("basepath");
         $user = $_SERVER["PHP_AUTH_USER"];
-        $eventHandler->register(EventHandler::EVENT_ADD_ROUTE, function(EventArgument $event) use ($base){
+        $eventHandler->register(EventHandler::EVENT_ADD_ROUTE, function (EventArgument $event) use ($base) {
             $route = $event->route;
-            if(!$event->isSubRoute){
+            if (!$event->isSubRoute) {
                 return;
             }
-            switch(true){
+            switch (true) {
                 case $route instanceof ILoadableRoute:
                     $route->prependUrl($base);
                     break;
@@ -100,7 +118,7 @@ class Routes extends Toil {
 
         $permissions = new Permissions;
         preg_match("/\/([^\/?]+)(\?.*)?$/m", $_SERVER["REQUEST_URI"], $matches);
-        if(!$permissions->checkPermissions($user, $matches[1])){
+        if (!$permissions->checkPermissions($user, $matches[1])) {
             Exceptions::error_rep("[API] Failed checking permissions for expected endpoint: " . $matches[1]);
             header("Content-type: application/json");
             header("HTTP/1.1 403 Forbidden");
@@ -109,86 +127,86 @@ class Routes extends Toil {
         }
 
         Router::addEventHandler($eventHandler);
-        Router::get("/api/v1/toil/getVersion", function(){
+        Router::get("/api/v1/toil/getVersion", function () {
             Exceptions::error_rep("Accessing 'getVersion' endpoint. User: " . $this->__get("api_username"));
             Controller::createview("getVersion");
         });
-        Router::get("/api/v1/toil/healthcheck", function(){
+        Router::get("/api/v1/toil/healthcheck", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'healthcheck' endpoint");
             Controller::createview("healthcheck");
         });
-        Router::get("/api/v1/toil/getUserCount", function(){
+        Router::get("/api/v1/toil/getUserCount", function () {
             Exceptions::error_rep("[API] User authenticated and acceessing 'getUserCount' endpoint");
             Controller::createview("getUserCount");
         });
-        Router::get("/api/v1/toil/getApiVersion", function(){
+        Router::get("/api/v1/toil/getApiVersion", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'getApiVersion' endpoint");
             Controller::createview("getApiVersion");
         });
-        Router::get("/api/v1/toil/getLog", function(){
+        Router::get("/api/v1/toil/getLog", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'getLog' endpoint");
             Controller::createview("getLog");
         });
-        Router::get("/api/v1/toil/getWorktimes", function(){
+        Router::get("/api/v1/toil/getWorktimes", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'getWorktimes' endpoint");
             Controller::createview("getWorktimes");
         });
 
-        Router::get("/api/v1/toil/getVacations", function(){
+        Router::get("/api/v1/toil/getVacations", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'getVacations' endpoint");
             Controller::createview("getVacations");
         });
 
-        Router::get("/api/v1/toil/getUsers", function(){
+        Router::get("/api/v1/toil/getUsers", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'getUsers' endpoint");
             Controller::createview("getUsers");
         });
 
-        Router::get("/api/v1/toil/getUserDetails", function(){
+        Router::get("/api/v1/toil/getUserDetails", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'getUserDetails' endpoint");
             Controller::createview("getUserDetails");
         });
 
-        Router::get("/api/v1/toil/approveVacation", function(){
+        Router::get("/api/v1/toil/approveVacation", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'approveVacation' endpoint");
             Controller::createview("approveVacation");
         });
 
-        Router::get("/api/v1/toil/addWorktime", function(){
+        Router::get("/api/v1/toil/addWorktime", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'addWorktime' endpoint");
             Controller::createview("addWorktime");
         });
 
-        Router::get("/api/v1/toil/addVacation", function(){
+        Router::get("/api/v1/toil/addVacation", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'addVacation' endpoint");
             Controller::createview("addVacation");
         });
 
-        Router::get("/api/v1/toil/addProject", function(){
+        Router::get("/api/v1/toil/addProject", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'addProject' endpoint");
             Controller::createview("addProject");
         });
-        
-        Router::get("/api/v1/toil/getUserWorktimes", function(){
+
+        Router::get("/api/v1/toil/getUserWorktimes", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'getUserWorktimes' endpoint");
             Controller::createview("getUserWorktimes");
         });
 
-        Router::get("/api/v1/toil/deleteWorktime", function(){
+        Router::get("/api/v1/toil/deleteWorktime", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'deleteWorktime' endpoint");
             Controller::createview("deleteWorktime");
         });
 
-        Router::get("/api/v1/toil/deleteUser", function(){
+        Router::get("/api/v1/toil/deleteUser", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'deleteUser' endpoint");
             Controller::createview("deleteUser");
         });
 
-        Router::get("/api/v1/toil/addUser", function(){
+        Router::get("/api/v1/toil/addUser", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'addUser' endpoint");
             Controller::createview("addUser");
         });
-        Router::get("/api/v1/toil/getOwnWorktime", function(){
+        Router::get("/api/v1/toil/getOwnWorktime", function () {
             Exceptions::error_rep("[API] User authenticated and accessing 'getOwnWorktime' endpoint");
             Controller::createview("getOwnWorktime");
         });
@@ -196,8 +214,8 @@ class Routes extends Toil {
         // Loading all custom routes
         CustomRoutes::loadCustomRoutes();
 
-        Router::error(function(Request $request, \Exception $e){
-            switch($e->getCode()){
+        Router::error(function (Request $request, \Exception $e) {
+            switch ($e->getCode()) {
                 case 404:
                     Exceptions::error_rep("[API] 404 Not found. Endpoint: " . $request->getUrl());
                     header("Content-type: application/json");
@@ -214,7 +232,8 @@ class Routes extends Toil {
         });
     }
 
-    public function getResourceNameFromPath($path){
+    public function getResourceNameFromPath($path)
+    {
         preg_match("/[^\/]+$/", $path, $match);
         return $match[0];
     }
